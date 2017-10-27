c6ee912d346196d2e221d98c71a40564df11c27c
Douglas Bates <dmbates@gmail.com>
2017-10-27 16:25:01 -0500

    Update dropXcolumn to copy original fit

--- src/profile.jl
+++ src/profile.jl
@@ -1,20 +1,16 @@
 """
-    dropXcolumn!(m::LinearMixedModel, i::Integer)
+    dropXcolumn(m::LinearMixedModel, i::Integer)
 
-Return `m` with column `i` of the fixed-effects model matrix, `X`, dropped
-
-This operation changes dimensions of some blocks in `m.A` and `m.L`
-FIXME: It looks as if A and L need to be generated anew.
+Return a copy of `m` with column `i` of the fixed-effects model matrix, `X`, dropped
 """
-function dropXcolumn!(m::LinearMixedModel, i::Integer)
-    trms = m.trms
+function dropXcolumn(m::LinearMixedModel, i::Integer)
+    trms = copy.(m.trms)
     xpos = length(trms) - 1
-    xtrm = trms[xpos] = reweight!(dropcolumn(trms[xpos], i), m.sqrtwts)
-    for j in 1:xpos
-        Axj = m.A[Block(xpos, j)] = xtrm'trms[j]
-        m.L[Block(xpos, j)] = deepcopy(Axj)
-    end
-    Axp1x = m.A[Block(xpos + 1, xpos)] = trms[xpos + 1]'xtrm
-    m.L[Block(xpos + 1, xpos)] = deepcopy(Axp1x)
-    m
+    trms[xpos] = dropcolumn(trms[xpos], i)
+    sqrtwts = copy(m.sqrtwts)
+    reweight!.(trms, [sqrtwts])
+    A, L = createAL(trms)   
+    osum = copy(m.optsum)
+    copy!(osum.initial, m.optsum.final)
+    LinearMixedModel(m.formula, trms, sqrtwts, A, LowerTriangular(L), osum)
 end
